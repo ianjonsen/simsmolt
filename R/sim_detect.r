166428fc6ff2c06bd9c611f981a47ba463abbe46
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-01-30 12:51:44 -0400

    add transmit df's to output

--- R/sim_detect.r
+++ R/sim_detect.r
@@ -29,7 +29,7 @@ sim_detect <-
         sqrt((r["x"] - xy[, "x"]) ^ 2 + (r["y"] - xy[, "y"]) ^ 2)
     in.rng <- which(dfn(mrec, s$sim[, c("x", "y")]) <= 10)
     if (length(in.rng) > 0)
-      tr_sobi <- trap(s$sim[in.rng, c("x", "y")] * 1000, delayRng = c(20, 60)) ## delay from Chaput et al 2018
+      tr_sobi <- sim_transmit(s$sim[in.rng, c("x", "y")] * 1000, delayRng = c(20, 60)) ## delay from Chaput et al 2018
    
     ## create 1 km buffer around Lab Sea array
     rb.buff <- s$data$labsea_poly %>% buffer(., 1)
@@ -38,7 +38,7 @@ sim_detect <-
     in.grid <- point.in.SpatialPolygons(s$sim$x, s$sim$y, rb.buff)
     if (sum(in.grid) > 0)
       ## FIXME: will return approx error if only 1 location in grid
-      tr_labsea <- trap(s$sim[in.grid, c("x", "y")] * 1000, delayRng = delay, burstDur = burst)
+      tr_labsea <- sim_transmit(s$sim[in.grid, c("x", "y")] * 1000, delayRng = delay, burstDur = burst)
     
     ## define logistic detection range (m) function
     ## parameterised from analysis of SoBI sentinel tag detections
@@ -63,15 +63,24 @@ sim_detect <-
                                      detRngFun = pdrf)
     }
     
-    if(exists("dt_sobi") & exists("dt_labsea")) {
-    s$detect <- bind_rows(dt_sobi, dt_labsea) %>%
-      mutate(array = rep(c("sobi", "labsea"), c(nrow(dt_sobi), nrow(dt_labsea)))) %>%
-      arrange(desc(array), etime, recv_id, trns_id)
-    } else if(exists("dt_sobi") & !exists("dt_labsea")) {
+    if (exists("dt_sobi") & exists("dt_labsea")) {
+      s$trans <- bind_rows(tr_sobi, tr_labsea) %>%
+        mutate(array = rep(c("sobi", "labsea"), c(nrow(tr_sobi), nrow(tr_labsea)))) %>%
+        arrange(desc(array), etime)
+        s$detect <- bind_rows(dt_sobi, dt_labsea) %>%
+          mutate(array = rep(c("sobi", "labsea"), c(nrow(dt_sobi), nrow(dt_labsea)))) %>%
+          arrange(desc(array), etime, recv_id, trns_id)
+    } else if (exists("dt_sobi") & !exists("dt_labsea")) {
+      s$trans <- tr_sobi %>%
+        mutate(array = rep(c("sobi"), c(nrow(tr_sobi)))) %>%
+        arrange(desc(array), etime)
       s$detect <- dt_sobi %>%
         mutate(array = rep(c("sobi"), c(nrow(dt_sobi)))) %>%
         arrange(desc(array), etime, recv_id, trns_id)
-    } else if(!exists("dt_sobi") & exists("dt_labsea")) {
+    } else if (!exists("dt_sobi") & exists("dt_labsea")) {
+      s$trans <- tr_labse %>%
+        mutate(array = rep(c("labsea"), c(nrow(tr_labsea)))) %>%
+  arrange(desc(array), etime)
       s$detect <- dt_labsea %>%
         mutate(array = rep(c("labsea"), c(nrow(dt_labsea)))) %>%
         arrange(desc(array), etime, recv_id, trns_id)
