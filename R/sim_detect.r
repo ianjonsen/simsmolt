dff242bbcd034cfe63cbf222665dca2cba70976a
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-01-31 11:35:02 -0400

    fix detection results so they work under any result scenario

--- R/sim_detect.r
+++ R/sim_detect.r
@@ -22,14 +22,15 @@ sim_detect <-
     ##  otherwise trap() output is far too big to generate along full track
     ##    - convert locs from km to m grid; vel in m/s
     ## mean loc for SoBI line
-    tr_sobi <- tr_labsea <- NULL
+    trans <- tmp.tr <- dt <- tmp.dt <- NULL
     mrec <- apply(s$data$sobi / 1000, 2, mean)
     dfn <-
       function(r, xy)
         sqrt((r["x"] - xy[, "x"]) ^ 2 + (r["y"] - xy[, "y"]) ^ 2)
     in.rng <- which(dfn(mrec, s$sim[, c("x", "y")]) <= 10)
     if (length(in.rng) > 0)
-      tr_sobi <- sim_transmit(s$sim[in.rng, c("x", "y")] * 1000, delayRng = c(20, 60)) ## delay from Chaput et al 2018
+      trans <- sim_transmit(s$sim[in.rng, c("x", "y")] * 1000, delayRng = delay, burstDur = burst) %>%
+        mutate(array = rep("sobi", nrow(.)))
    
     ## create 1 km buffer around Lab Sea array
     rb.buff <- s$data$labsea_poly %>% buffer(., 1)
@@ -38,7 +39,10 @@ sim_detect <-
     in.grid <- point.in.SpatialPolygons(s$sim$x, s$sim$y, rb.buff)
     if (sum(in.grid) > 0)
       ## FIXME: will return approx error if only 1 location in grid
-      tr_labsea <- sim_transmit(s$sim[in.grid, c("x", "y")] * 1000, delayRng = delay, burstDur = burst)
+      tmp.tr <- sim_transmit(s$sim[in.grid, c("x", "y")] * 1000, delayRng = delay, burstDur = burst) %>%
+        mutate(array = rep("labsea", nrow(.)))
+    
+    trans <- bind_rows(trans, tmp.tr) 
     
     ## define logistic detection range (m) function
     ## parameterised from analysis of SoBI sentinel tag detections
@@ -46,44 +50,34 @@ sim_detect <-
     pdrf <- function(dm, b = c(3.017, -0.0139)) {
       plogis(b[1] + b[2] * dm)
     }
-    
+ 
     ## simulate detections given receiver locations & simulated transmission along track
     ##  FIXME: NEED TO ADAPT GLATOS VERSION SO PDRF CAN BE MODIFIED IN FN CALL
-    dt_sobi <- dt_labsea <- NULL
-    if (!is.null(tr_sobi)) {
-      dt_sobi <-
-        detect_transmissions(trnsLoc = tr_sobi,
-                                     recLoc = s$data$sobi,
-                                     detRngFun = pdrf)
-    }
-    if (!is.null(tr_labsea)) {
-      dt_labsea <-
-        detect_transmissions(trnsLoc = tr_labsea,
-                                     recLoc = s$data$labsea,
-                                     detRngFun = pdrf)
-    }
-    
-    if (exists("dt_sobi") & exists("dt_labsea")) {
-      s$trans <- bind_rows(tr_sobi, tr_labsea) %>%
-        mutate(array = rep(c("sobi", "labsea"), c(nrow(tr_sobi), nrow(tr_labsea)))) %>%
+
+    if(nrow(trans) > 0) {
+      dt <- trans %>%
+        filter(array == "sobi") %>%
+        detect_transmissions(trnsLoc = .,
+                            recLoc = s$data$sobi,
+                            detRngFun = pdrf) %>%
+        mutate(array = rep("sobi", nrow(.)))
+      
+      if(!is.null(tmp.tr)) {
+        tmp.dt <- trans %>%
+          filter(array == "labsea") %>%
+          detect_transmissions(trnsLoc = .,
+                               recLoc = s$data$labsea,
+                               detRngFun = pdrf) %>%
+          mutate(array = rep("labsea", nrow(.)))
+      }
+      
+      dt <- bind_rows(dt, tmp.dt)
+      s$trans <- trans %>%
         arrange(desc(array), et)
-        s$detect <- bind_rows(dt_sobi, dt_labsea) %>%
-          mutate(array = rep(c("sobi", "labsea"), c(nrow(dt_sobi), nrow(dt_labsea)))) %>%
-          arrange(desc(array), etime, recv_id, trns_id)
-    } else if (exists("dt_sobi") & !exists("dt_labsea")) {
-      s$trans <- tr_sobi %>%
-        mutate(array = rep(c("sobi"), c(nrow(tr_sobi)))) %>%
-        arrange(desc(array), et)
-      s$detect <- dt_sobi %>%
-        mutate(array = rep(c("sobi"), c(nrow(dt_sobi)))) %>%
-        arrange(desc(array), etime, recv_id, trns_id)
-    } else if (!exists("dt_sobi") & exists("dt_labsea")) {
-      s$trans <- tr_labse %>%
-        mutate(array = rep(c("labsea"), c(nrow(tr_labsea)))) %>%
-  arrange(desc(array), et)
-      s$detect <- dt_labsea %>%
-        mutate(array = rep(c("labsea"), c(nrow(dt_labsea)))) %>%
+      
+      s$detect <- dt %>%
         arrange(desc(array), etime, recv_id, trns_id)
     }
-    s
+ 
+    return(s)
   }
