1f33062b05c0c22f9bfcbac09fb2b7f9f398a9a3
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-01-31 13:57:19 -0400

    alter plot for SoBI start point

--- R/plot.simsmolt.R
+++ R/plot.simsmolt.R
@@ -3,7 +3,7 @@
 ##' @title plot
 ##' @param s a fitted object of class simsmolt
 ##' 
-##' @importFrom ggplot2 ggplot coord_cartesian geom_raster aes theme_minimal geom_point
+##' @importFrom ggplot2 ggplot coord_fixed geom_raster aes theme_minimal geom_point scale_color_brewer scale_fill_gradient2
 ##' @importFrom raster rasterToPoints
 ##' @method plot simsmolt
 ##' @export
@@ -14,14 +14,16 @@ bathy <- rasterToPoints(s$data$bathy) %>% data.frame()
 names(bathy)[3] <- "z"
   
 m <- ggplot() + 
-  coord_cartesian(xlim = c(s$data$tag[1], s$data$coa[1]), ylim = c(s$data$tag[2], s$data$coa[2])) + 
+  coord_fixed(ratio = 1.84, xlim = c(s$data$tag[1], s$data$coa[1]), ylim = c(s$data$tag[2], max(s$sim$y, na.rm = TRUE) * 1.1)) + 
   geom_raster(data = bathy, aes(x, y, fill = z)) + 
+  scale_fill_gradient2(low = "#053061", mid = "#43a2ca", high = "#e0f3db", guide = "none") +
   theme_minimal() + 
-  geom_point(data = s$sim, aes(x, y), colour = "yellow", size = 0.025) +
-  geom_point(data = s$data$sobi, aes(x/1000, y/1000), colour = "red", size = 0.01) +
-  geom_point(data = s$data$labsea, aes(x/1000, y/1000), colour = "red", size = 0.01)
+  geom_point(data = s$sim, aes(x, y), colour = "firebrick", size = 0.025) +
+  geom_point(data = s$data$sobi, aes(x/1000, y/1000), colour = "#2166ac", size = 0.01) +
+  geom_point(data = s$data$labsea, aes(x/1000, y/1000), colour = "#2166ac", size = 0.01)
    if(!is.null(s$detect)) {
-     m <- m + geom_point(data = s$detect, aes(recv_x/1000, recv_y/1000, colour = array), alpha = 0.45) 
+     m <- m + geom_point(data = s$detect, aes(recv_x/1000, recv_y/1000, colour = array)) +
+       scale_color_brewer(type = "qual", palette = 1)
    }
 
 print(m)
