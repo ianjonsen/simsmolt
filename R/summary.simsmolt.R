5aac9c7da69f0540f291dbe07bd759fd745a504b
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-01-31 11:35:27 -0400

    get summary printing to console properly

--- R/summary.simsmolt.R
+++ R/summary.simsmolt.R
@@ -1,30 +1,31 @@
-##' @importFrom dplyr %>%
+##' @importFrom dplyr %>% summarise group_by n
+##' @importFrom prevR point.in.SpatialPolygons
 ##' @method summary simsmolt
 ##' @export
-summary.simsmolt <- function(s, ...) {
+summary.simsmolt <- function(x, ...) {
   if (length(list(...)) > 0) {
     warning("additional arguments ignored")
   }
   
   ## num h smolt is in Lab Sea array
   h.in.grid <-
-    point.in.SpatialPolygons(s$sim$x, s$sim$y, s$data$labsea_poly) %>%
+    point.in.SpatialPolygons(x$sim$x, x$sim$y, x$data$labsea_poly) %>%
     sum()
-  tr <- s$trans %>% group_by(array) %>%
-    summarise(n())
-  dt <- s$detect %>% group_by(array) %>%
-    summarise(n())
+  tr <- x$trans %>% group_by(array) %>%
+    summarise(tr=n())
+  dt <- x$detect %>% group_by(array) %>%
+    summarise(dt=n())
 
-  mrec <- apply(s$data$sobi, 2, mean)
-  time2sobi <- which(s$sim$y > mrec[2])[1] / 24 ## time in days
+  mrec <- apply(x$data$sobi, 2, mean) / 1000
+  time2sobi <- which(x$sim$y > mrec[2])[1] / 24 ## time in days
   
   structure(list(
     time2sobi = time2sobi,
     h.in.grid = h.in.grid,
     trans = tr,
-    detections = dt
+    dets = dt
   ),
-  class = "simsmolt")
+  class = "summary.simsmolt")
   
 }
 
@@ -33,11 +34,11 @@ summary.simsmolt <- function(s, ...) {
 print.summary.simsmolt <- function(x, digits = 3,
                                ...)
 {
-  
+  browser()
   cat(sprintf("%3.0f d to reach SoBI\n", x$time2sobi/24))
   cat(sprintf("%3.0d h in Lab Sea grid\n", x$h.in.grid))
-  cat(sprintf("%3.0i total transmissions\n", x$trans))
-  cat(sprintf("%3.0i total detections\n", x$detections))
+  cat(sprintf("%3.0i total transmissions\n", x$trans$tr))
+  cat(sprintf("%3.0i total detections\n", x$dets$dt))
   
   invisible(x)
 }## print.summary.simsmolt
