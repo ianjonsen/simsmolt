e216d388b31ff1253e30f998d76becf166deb3d4
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-02-01 10:39:49 -0400

    ifelse causes only 1 draw from rweibull

--- R/rw.R
+++ R/rw.R
@@ -8,9 +8,13 @@
 #' 
 rw <- function(n = 1, mu = 0, rho = 0.5, a = 3, b = 5){
   
-  s <- ifelse(a > 0, rweibull(n, a, b), b)
+  if (a > 0)
+    st <- rweibull(n, a, b)
+  else {
+    st <- b
+  }
   
   phi <- rwrpcauchy(n, mu, rho)
-  
-  cbind(sin(phi) * s, cos(phi) * s)
+
+  cbind(sin(phi) * st, cos(phi) * st)
 } 
